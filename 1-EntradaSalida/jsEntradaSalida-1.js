7642fdea9324068b66da82b31f28283121cb2a3e
NahueFacello <60549384+NahuelFacello@users.noreply.github.com>
2020-02-04 19:04:32 -0300

    Mi primer commit
    Mi primer commit

--- 1-EntradaSalida/jsEntradaSalida-1.js
+++ 1-EntradaSalida/jsEntradaSalida-1.js
@@ -1,6 +1,14 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'mostrar'.
 function mostrar()
 {
-	alert("ok");
+	//var nombre;
+	//nombre=prompt ("Hola","Harry Potter");
+	//alert ("Harry Potter") Esto no
+	//alert(nombre)//escribi la variable que necesito para que luego lo arroje en la alerta
+	//alert("Esto funciona de maravilla");
+	var nombre 
+	nombre=prompt("Por favor ingresa tu nombre");
+	alert("Hola " + nombre);
+	var edad=prompt("Por favor ingresa tu edad")
+	alert("Su edad es "+edad+" Como estas hoy?");
 }
-
