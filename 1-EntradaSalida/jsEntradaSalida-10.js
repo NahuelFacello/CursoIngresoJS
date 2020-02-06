c7a8638bca38d7c5dffbfcc3ba36a2fd42840782
NahueFacello <nahuel-facello@live.com.ar>
2020-02-05 23:59:23 -0300

    Update jsEntradaSalida-10.js

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,22 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	
+	var importe;
+	var descuento;
+	var resultado; 
+
+		importe=document.getElementById('importe').value;
+		importe=parseInt(importe);
+
+		descuento=prompt("Ingrese el porcentaje de descuento a aplicar");
+		descuento=importe*descuento/100;
+
+		resultado=importe-descuento;
+
+		alert("El nuevo precio con el descuento aplicado es $"+resultado);
+
+		document.getElementById('resultado').value=resultado;
+
+
+
 }
