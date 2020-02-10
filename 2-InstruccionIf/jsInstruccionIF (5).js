80880b9523b818655573b6cb1d478b8557c0d8ec
NahueFacello <60549384+NahuelFacello@users.noreply.github.com>
2020-02-10 20:37:07 -0300

    IF - Ejercicio 5 -10022020

--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -1,6 +1,11 @@
 function mostrar()
 {
+	var edad;
+	edad=document.getElementById('edad').value;
 //tomo la edad  
-
+	if(edad<13||edad>18)
+	{
+		alert("No es adolescente");
+	}
 
 }//FIN DE LA FUNCIÓN
