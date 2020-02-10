940b7505d5d04cf312e346ec77a9cb25fb54d033
NahueFacello <60549384+NahuelFacello@users.noreply.github.com>
2020-02-10 20:36:35 -0300

    IF - Ejercicio 4 -10022020

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -1,7 +1,21 @@
 function mostrar()
 {
+	var edad;
+	edad=document.getElementById('edad').value;
 //tomo la edad  
+	/*if(edad<18&&edad>12)
+		{
+			alert("Es adolescente")
+		} Tener en cuenta que es lo mismo pero si quiero hacer algo entre los conjuntos, no lo puedo hacer.
+		*/
 
+	if(edad<18)
+	{
+		if(edad>12)
+		{
+			alert("Es adolescente")
+		}
+	}
 
 
 }//FIN DE LA FUNCIÓN
