dec0cc8f9f22a5b1adb1f7579497f02498514792
NahueFacello <60549384+NahuelFacello@users.noreply.github.com>
2020-02-10 22:01:38 -0300

    IF - Ejercicio 7 - 10022020

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -1,8 +1,13 @@
 function mostrar()
 {
-//tomo la edad  
-
-	
-
-
-}//FIN DE LA FUNCIÓN
+	var edad;
+	var estado;
+	estado=document.getElementById('estadoCivil').value;
+	edad=document.getElementById('edad').value;
+		if(edad<18&&estado!='Soltero')
+		{
+		alert("Es muy pequeño para NO ser soltero.");
+		}
+}
+//FIN DE LA FUNCIÓN
+//tomo la edad 
