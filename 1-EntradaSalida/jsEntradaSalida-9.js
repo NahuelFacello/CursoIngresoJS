37007fb535fbf20ec5b66d7ac6da83fb53023da1
NahueFacello <nahuel-facello@live.com.ar>
2020-02-16 20:23:07 -0300

    Ejercicio 9 EyS

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,18 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
-
-	
+	/*var sueldo;
+	var incremento;
+	sueldo=document.getElementById('sueldo').value;
+	sueldo=parseInt(sueldo);
+	incremento=10/100;
+	document.getElementById('resultado').value=sueldo+(sueldo*incremento);
+	console.log*/
+	var sueldo;
+	var incremento;
+	sueldo=document.getElementById('sueldo').value;
+	sueldo=parseInt(sueldo);
+	incremento=prompt("Ingrese el incremento %");
+	incremento=incremento/100;
+	document.getElementById('resultado').value=sueldo+(sueldo*incremento);
 }
