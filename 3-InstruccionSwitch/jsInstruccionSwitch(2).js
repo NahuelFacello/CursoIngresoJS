7f2cd4f5f061feae75d7dacfeff7e8ceebb2b309
NahueFacello <60549384+NahuelFacello@users.noreply.github.com>
2020-02-17 21:55:22 -0300

    Ejercicio3 - Switch

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -1,11 +1,22 @@
 function mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
-
-alert (mesDelAño);
-
-
-
-
+	var mesDelAño;
+	mesDelAño= document.getElementById('mes').value;
+	switch(mesDelAño)
+	{
+		case "Julio":
+		case "Agosto":
+			alert("Abrigate que hace frio");
+			break;
+		case "Septiembre":
+		case "Octubre":
+		case "Noviembre":
+		case "Diciembre":
+			alert("Ya pasamos el frio, ahora calor!!!");
+			break;
+		default:
+			alert("Falta para el invierno");
+			break;
+	}
 }//FIN DE LA FUNCIÓN
