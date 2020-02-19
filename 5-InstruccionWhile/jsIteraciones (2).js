3dcd3342ef22f94c0a9d40f39e7908f8c433141e
Nahuel <you@example.com>
2020-02-18 21:59:30 -0300

    Ejercicios While

--- 5-InstruccionWhile/jsIteraciones (2).js
+++ 5-InstruccionWhile/jsIteraciones (2).js
@@ -1,6 +1,12 @@
 function mostrar()
 {
-	alert('iteración while');
+	contador=10;
+	while(contador>0)
+	{
+		console.log(contador);
+		contador=contador-1;
+	}
+	//alert('iteración while');
 
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -1,7 +1,12 @@
 function mostrar()
 {
 
-var clave = prompt("ingrese el número clave.");
-
+	var clave;
+	clave= prompt("ingrese el número clave.");
+	while(clave!="utn750")
+	{
+		clave=prompt("Error, ingrese nuevamente la clave");
+	}
+	alert("Bienvenido");
 
 }//FIN DE LA FUNCIÓN
