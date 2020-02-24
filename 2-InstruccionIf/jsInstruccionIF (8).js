2ed76b780a1f7f591e0393664ead006678421cbe
NahueFacello <nahuel-facello@live.com.ar>
2020-02-23 21:44:35 -0300

    Ejercicio 8 If

--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -2,7 +2,14 @@ function mostrar()
 {
 //tomo la edad  
 
-	
-
+	var edad;
+	var estado;
+	edad=document.getElementById('edad').value;
+	estado=document.getElementById('estadoCivil').value;
+	if(edad>18&&estado=="Soltero")
+	{
+		alert("Es soltero y no es menor.")
+	}
+console.log;
 
 }//FIN DE LA FUNCIÓN
